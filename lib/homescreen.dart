251a651288955a92c9e56480d2261ba634232c48
Abdur Rehman <manikhan5353@gmail.com>
2022-12-26 12:17:52 +0500

    Adjusted elevation offsets and colors with sliders

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -2,18 +2,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// class MyHomePage extends GetxController {
-//   const MyHomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
+var _yOffset = 10.0;
+var _xOffset = 10.0;
+var _blurAmount = 10.0;
 
-class MyHomePage extends StatelessWidget {
-  var yOffset = 10.0.obs;
-  var xOffset = 10.0.obs;
-  var blurAmount = 10.0.obs;
-  var layerCount = 10.0.obs;
+var _red = 10;
+var _green = 10;
+var _blue = 10;
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -25,47 +27,80 @@ class MyHomePage extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Obx(() => Column(
-            children: [
-              slider(yOffset.value, "Y Offset"),
-              slider(xOffset.value, "X Offset"),
-              slider(blurAmount.value, "Blur Amount"),
-              slider(layerCount.value, "Layer Count"),
-              FlyingContainer(size, blurAmount.value, xOffset.value, yOffset.value)
-            ],
-          ))
-
+          slider(_yOffset, "Y Offset",
+              min: 1.0,
+              max: 40.0,
+              callback: (yOffset) => setState(() {
+                    _yOffset = yOffset;
+                  })),
+          slider(_xOffset, "X Offset",
+              min: 1.0,
+              max: 40.0,
+              callback: (xOffset) => setState(() {
+                    _xOffset = xOffset;
+                  })),
+          slider(_blurAmount, "Blur Amount",
+              min: 1.0,
+              max: 50.0,
+              callback: (blurAmount) => setState(() {
+                    _blurAmount = blurAmount;
+                  })),
+          slider(_red.toDouble(), "Red",
+              min: 1.0,
+              max: 255.0,
+              callback: (red) => setState(() {
+                    _red = red.toInt();
+                  })),
+          slider(_green.toDouble(), "Green",
+              min: 1.0,
+              max: 255.0,
+              callback: (green) => setState(() {
+                    _green = green.toInt();
+                  })),
+          slider(_blue.toDouble(), "Blue",
+              min: 1.0,
+              max: 255.0,
+              callback: (blue) => setState(() {
+                    _blue = blue.toInt();
+                  })),
+          FlyingContainer(
+            size,
+            _blurAmount,
+            _xOffset,
+            _yOffset,
+            red: _red,
+            blue: _blue,
+            green: _green,
+          )
         ],
       ),
     );
   }
 }
 
-class FlyingContainer extends StatefulWidget {
-  FlyingContainer(this.size, this.blurAmount, this.xOffset, this.yOffset, {Key? key}) : super(key: key);
-  var size, blurAmount, xOffset, yOffset;
+class FlyingContainer extends StatelessWidget {
+  FlyingContainer(this.size, this.blurAmount, this.xOffset, this.yOffset,
+      {Key? key, this.red, this.green, this.blue})
+      : super(key: key);
+  var size, blurAmount, xOffset, yOffset, red, green, blue;
 
-  @override
-  State<FlyingContainer> createState() => _FlyingContainerState();
-}
-
-class _FlyingContainerState extends State<FlyingContainer> {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: const BorderRadius.all(Radius.circular(15.0)),
       child: Container(
-        margin: EdgeInsets.all(20),
-        height: widget.size.height * 0.1,
-        width: widget.size.width * 0.6,
+        margin: EdgeInsets.all(50),
+        height: size.height * 0.1,
+        width: size.width * 0.6,
         decoration: BoxDecoration(
-            color: Colors.white,
+            color: Color.fromRGBO(red, green, blue, 1),
             borderRadius: BorderRadius.all(Radius.circular(15.0)),
+
             boxShadow: [
               BoxShadow(
-                blurRadius: widget.blurAmount,
-                color: Colors.blue,
-                offset: Offset(widget.xOffset, widget.yOffset),
+                blurRadius: blurAmount,
+                color: Color.fromRGBO(red, green, blue, 1),
+                offset: Offset(xOffset, yOffset),
               ),
             ]),
       ),
@@ -73,11 +108,12 @@ class _FlyingContainerState extends State<FlyingContainer> {
   }
 }
 
-
 class slider extends StatefulWidget {
   double value;
   String heading;
-  slider(this.value, this.heading);
+  var callback;
+  var min, max;
+  slider(this.value, this.heading, {this.callback, this.min, this.max});
 
   @override
   State<slider> createState() => _sliderState();
@@ -98,7 +134,7 @@ class _sliderState extends State<slider> {
                 style: const TextStyle(color: Colors.black, fontSize: 20),
               ),
               Text(
-                "${widget.value.round()}/100",
+                "${widget.value.round()}/${widget.max.round()}",
                 style: const TextStyle(
                     fontStyle: FontStyle.italic, color: Colors.grey),
               ),
@@ -108,12 +144,13 @@ class _sliderState extends State<slider> {
         SizedBox(
           width: MediaQuery.of(context).size.width - 30,
           child: Slider(
-              min: 1.0,
-              max: 100.0,
+              min: widget.min,
+              max: widget.max,
               value: widget.value,
               onChanged: (val) {
                 setState(() {
                   widget.value = val;
+                  widget.callback(val);
                 });
               }),
         ),
